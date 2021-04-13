dde157c0c4c7bfe9fbf7b605bf28359e6779d4fe
takeItIzzy <q1367257497@gmail.com>
2021-04-13 12:57:50 +0800

    improvement(banner): 添加一张模糊背景图，提高展示效果

--- components/layout/banner.tsx
+++ components/layout/banner.tsx
@@ -6,14 +6,27 @@ const Banner = ({ title }: { title: ReactNode }) => {
   return (
     <section className={`h-80 flex-center relative mb-12 ${styles.banner}`}>
       <div className="absolute t-0 l-0 w-full h-full">
-        <Image src="/bg.jpg" layout="fill" objectFit="cover" objectPosition="center" />
+        <Image
+          src="/bg-low-quality.jpg"
+          alt="背景图片"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+        <Image
+          src="/bg.jpg"
+          alt="背景图片"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
       </div>
       <h1 className="text-white text-center leading-normal z-10 font-light animate-banner-title">
         {title}
       </h1>
       <div className="w-full max-w-5xl absolute bottom-0 z-10">
         <div className="absolute -bottom-12 left-6 w-24 h-24 border-4 rounded-full border-white overflow-hidden">
-          <Image src="/avatar.jpg" layout="fill" />
+          <Image src="/avatar.jpg" layout="fill" alt="头像" />
         </div>
       </div>
     </section>
