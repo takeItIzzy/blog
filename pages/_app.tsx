1e04d6c895b9fba976f42d6bed0654fb9b799d5f
takeItIzzy <q1367257497@gmail.com>
2021-08-15 14:49:09 +0800

    feat: 支持 pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'theme/ThemeContext';
 import 'styles/globals.css';
+import { Head } from 'next/document';
 
 const MyBlog = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
