bd9a0c033f0ee493a8c24f363bc986cdc8e6535b
takeItIzzy <48763672+takeItIzzy@users.noreply.github.com>
2023-07-12 15:55:41 +0800

    feat(sitemap): method generateSitemap supports generate dynamic route

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -3,20 +3,36 @@ const globby = require('globby');
 
 const generateSitemap = async () => {
   const pages = await globby([
-    'pages/**/*.{ts,tsx}',
-    // '!pages/**/[*.{ts,tsx}', // 忽略动态路由
+    'pages/**/*.{ts,tsx,mdx}',
     '!pages/_*.{ts,tsx}', // 忽略 next _app _document
     '!pages/api', // 忽略 api 路由
   ]);
 
+  const posts = await globby('posts/*.md');
+
   const urlSet = pages
-    .map((page) => {
-      const path = page
+    .reduce((prev, curr) => {
+      // 静态路由
+      if (!curr.includes('[')) {
+        const path = page
         .replace('pages', '')
-        .replace(/(\.tsx|\.ts)/, '')
+        .replace(/(\.tsx|\.ts|\.mdx)/, '')
         .replace('/index', '');
-      return `<url><loc>https://takeitizzy.com${path}</loc></url>`;
-    })
+      return prev.concat([`<url><loc>https://takeitizzy.com${path}</loc></url>`]);
+      }
+
+      // 动态路由，去 /posts 文件夹下读取所有 md 文件名进行填充
+      const dynamicRoutes = dynamicPages.map((dynamicPage) => {
+        const id = dynamicPage.replace('.md', '');
+        const dynamicPath = curr
+          .replace('pages', '')
+          .replace('[id]', `${id}`)
+          .replace(/(\.tsx|\.ts|\.mdx)/, '')
+        return `<url><loc>https://takeitizzy.com${dynamicPath}</loc></url>`;
+      });
+
+      return prev.concat(dynamicRoutes);
+    }, [])
     .join('');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlSet}</urlset>`;
